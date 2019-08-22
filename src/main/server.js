9b8a72dc89331665d8bfe5142e169df5c54825ba
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-08-22 15:34:10 +0200

    formating documents

--- src/main/server.js
+++ src/main/server.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 app.use(express.json());
-
-app.use('/', routes);
-
+app.use("/", routes);
 
 module.exports = app;
